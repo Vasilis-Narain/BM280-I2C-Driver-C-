2c35ed2416d3f7b2a8b4292e8e294f5c18ada741
vasilis <vasilis.narain@hotmail.it>
2026-08-31 17:34:16 +0200

    feat(vasilis): set up clk_ref and clk_sys to be XOSC. I think setup is ready for writing the driver. Next steps are to test what we can read from it before trying to make the state machine.

--- src/_crt0.c
+++ src/_crt0.c
@@ -1,4 +1,8 @@
 #include <type_alias.h>
+#include <hardware/address_mapped.h>
+#include <hardware/structs/clocks.h>
+#include <hardware/regs/clocks.h>
+#include <hardware/structs/xosc.h>
 
 extern u8 __data_start;
 extern u8 __data_end;
@@ -39,5 +43,22 @@ void _crt0() {
     }
 }
 
-static void _config_ref_clock() {}
-static void _config_sys_clock() {}
+static void _config_ref_clock() {
+    clocks_hw->clk[clk_ref].ctrl = (clocks_hw->clk[clk_ref].ctrl & ~CLOCKS_CLK_REF_CTRL_SRC_BITS) |
+                                   CLOCKS_CLK_REF_CTRL_SRC_VALUE_XOSC_CLKSRC;
+
+    clocks_hw->clk[clk_ref].div = 1 << 16;
+
+    while ((clocks_hw->clk[clk_ref].selected & (1 << CLOCKS_CLK_REF_CTRL_SRC_VALUE_XOSC_CLKSRC)) == 0) {}
+}
+static void _config_sys_clock() {
+    clocks_hw->resus.ctrl = 0;
+
+    xosc_hw->ctrl = XOSC_CTRL_FREQ_RANGE_VALUE_1_15MHZ;
+    xosc_hw->startup = 47;
+    hw_set_bits(&xosc_hw->ctrl, XOSC_CTRL_ENABLE_VALUE_ENABLE << XOSC_CTRL_ENABLE_LSB);
+    while (((xosc_hw->status) & XOSC_STATUS_STABLE_BITS) == 0) {}
+
+    hw_clear_bits(&clocks_hw->clk[clk_sys].ctrl, CLOCKS_CLK_SYS_CTRL_SRC_BITS);
+    while (!(clocks_hw->clk[clk_sys].selected & (1 << CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLK_REF))) {}
+}
--- src/main.c
+++ src/main.c
@@ -8,19 +8,49 @@
 
 #include "driver/addresses.h"
 
-#define CYCLES 1000000
+#define CYCLES 1700000 / 2
 #define PIN25 25
 #define SDA 14
 #define SCL 15
+
+// I2C Control Register
+// 0x00000400 [10]    STOP_DET_IF_MASTER_ACTIVE (0) Master issues the STOP_DET interrupt irrespective of...
+// 0x00000200 [9]     RX_FIFO_FULL_HLD_CTRL (0) This bit controls whether DW_apb_i2c should hold the bus...
+// 0x00000100 [8]     TX_EMPTY_CTRL (0) This bit controls the generation of the TX_EMPTY...
+// 0x00000080 [7]     STOP_DET_IFADDRESSED (0) In slave mode: - 1'b1:  issues the STOP_DET interrupt...
+// 0x00000040 [6]     IC_SLAVE_DISABLE (1) This bit controls whether I2C has its slave disabled,...
+// 0x00000020 [5]     IC_RESTART_EN (1) Determines whether RESTART conditions may be sent when...
+// 0x00000010 [4]     IC_10BITADDR_MASTER (0) Controls whether the DW_apb_i2c starts its transfers in...
+// 0x00000008 [3]     IC_10BITADDR_SLAVE (0) When acting as a slave, this bit controls whether the...
+// 0x00000006 [2:1]   SPEED        (0x2) These bits control at which speed the DW_apb_i2c...
+// 0x00000001 [0]     MASTER_MODE  (1) This bit controls whether the DW_apb_i2c master is enabled
+#define I2C_INIT_MASK (I2C_IC_CON_MASTER_MODE_VALUE_ENABLED |                    \
+                       I2C_IC_CON_SPEED_VALUE_STANDARD << I2C_IC_CON_SPEED_LSB | \
+                       I2C_IC_CON_IC_RESTART_EN_BITS | I2C_IC_CON_IC_SLAVE_DISABLE_BITS)
+
+// 0x00000100 [8]     ISO          (1) Pad isolation control
+// 0x00000080 [7]     OD           (0) Output disable
+// 0x00000040 [6]     IE           (0) Input enable
+// 0x00000030 [5:4]   DRIVE        (0x1) Drive strength
+// 0x00000008 [3]     PUE          (0) Pull up enable
+// 0x00000004 [2]     PDE          (1) Pull down enable
+// 0x00000002 [1]     SCHMITT      (1) Enable schmitt trigger
+// 0x00000001 [0]     SLEWFAST     (0) Slew rate control
+#define PADS_I2C_CLEAR (PADS_BANK0_GPIO0_ISO_BITS | \
+                        PADS_BANK0_GPIO0_PDE_BITS | \
+                        PADS_BANK0_GPIO0_PUE_BITS)
+
+#define PADS_I2C_SET (PADS_BANK0_GPIO0_IE_BITS)
+
+#define RESETS_MASK (RESETS_RESET_IO_BANK0_BITS | RESETS_RESET_PADS_BANK0_BITS | RESETS_RESET_I2C1_BITS)
 
 // Going to avoid making abstractions as much as possible to have this as a reference of
 // order of operations. However, using the provided `*.h` instead of handcoding
 // as irl I'd mostly be taking definitions from manufacturer (svd).
 void main() {
-    const u32 reset_mask = RESETS_RESET_IO_BANK0_BITS | RESETS_RESET_PADS_BANK0_BITS;
 
-    hw_clear_bits(&resets_hw->reset, reset_mask);
-    while ((resets_hw->reset_done & reset_mask) != reset_mask) {}
+    hw_clear_bits(&resets_hw->reset, RESETS_MASK);
+    while ((resets_hw->reset_done & RESETS_MASK) != RESETS_MASK) {}
 
     //io_bank0_hw
     io_bank0_hw->io[PIN25].ctrl = GPIO_FUNC_SIO;
@@ -28,20 +58,12 @@ void main() {
     io_bank0_hw->io[SCL].ctrl = GPIO_FUNC_I2C;
 
     // Pads bank
+    hw_clear_bits(&pads_bank0_hw->io[PIN25], PADS_BANK0_GPIO0_ISO_BITS);
 
-    // 0x00000100 [8]     ISO          (1) Pad isolation control
-    // 0x00000080 [7]     OD           (0) Output disable
-    // 0x00000040 [6]     IE           (0) Input enable
-    // 0x00000030 [5:4]   DRIVE        (0x1) Drive strength
-    // 0x00000008 [3]     PUE          (0) Pull up enable
-    // 0x00000004 [2]     PDE          (1) Pull down enable
-    // 0x00000002 [1]     SCHMITT      (1) Enable schmitt trigger
-    // 0x00000001 [0]     SLEWFAST     (0) Slew rate control
-    hw_clear_bits(&pads_bank0_hw->io[PIN25], PADS_BANK0_GPIO25_ISO_BITS);
-
-    const u32 i2c_mask = (1 << 6) | (1 << 1);
-    hw_set_bits(&pads_bank0_hw->io[SDA], i2c_mask);
-    hw_set_bits(&pads_bank0_hw->io[SCL], i2c_mask);
+    hw_clear_bits(&pads_bank0_hw->io[SDA], PADS_I2C_CLEAR);
+    hw_clear_bits(&pads_bank0_hw->io[SCL], PADS_I2C_CLEAR);
+    hw_set_bits(&pads_bank0_hw->io[SDA], PADS_I2C_SET);
+    hw_set_bits(&pads_bank0_hw->io[SCL], PADS_I2C_SET);
 
     // Config I2C1 as master
     // I2C Control Register
@@ -56,9 +78,20 @@ void main() {
     // 0x00000006 [2:1]   SPEED        (0x2) These bits control at which speed the DW_apb_i2c...
     // 0x00000001 [0]     MASTER_MODE  (1) This bit controls whether the DW_apb_i2c master is enabled
     i2c1_hw->enable = 0;
-    i2c1_hw->con = 1 | (1 << 1) | (1 << 5) | (1 << 6);
+    i2c1_hw->con = I2C_INIT_MASK;
     i2c1_hw->tar = BME280_I2C_ADDR;
+
+    //TODO: set ic_clk and related. Must be set before enabling
+
     i2c1_hw->enable = 1;
+
+    // I2C Rx/Tx Data Buffer and Command Register
+    // 0x00000800 [11]    FIRST_DATA_BYTE (0) Indicates the first data byte received after the address...
+    // 0x00000400 [10]    RESTART      (0) This bit controls whether a RESTART is issued before the...
+    // 0x00000200 [9]     STOP         (0) This bit controls whether a STOP is issued after the...
+    // 0x00000100 [8]     CMD          (0: write) This bit controls whether a read or a write is performed
+    // 0x000000ff [7:0]   DAT          (0x00) This register contains the data to be transmitted or...
+    i2c1_hw->data_cmd = I2C_IC_DATA_CMD_CMD_BITS; // read command
 
     sio_hw->gpio_oe_set = 1 << PIN25;
 
